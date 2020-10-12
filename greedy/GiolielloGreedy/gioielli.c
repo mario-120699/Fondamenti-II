39eb27fa4c4d6a34aaa3f6d9fd47aef732b2c8e9
mario-120699 <bmtotaro90@gmail.com>
2020-10-12 09:25:51 +0200

    gioielli

--- greedy/GiolielloGreedy/gioielli.c
+++ greedy/GiolielloGreedy/gioielli.c
@@ -4,7 +4,10 @@
 
 Gioiello *Gioielli(const char * filename, float b, int * ret_size)
 {
+	char *buf = calloc(200, sizeof(char));
 	FILE *f = fopen("gioielli.txt", "r");
 	if(!f) // controllo se il file esiste e se non ci sono problemi all'apertura
 		return NULL;
+
+	fclose(f);
 }
